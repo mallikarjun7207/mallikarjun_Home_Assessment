b8151046d77e09f9d5687676c179028932792135
Mallikarjun <mallikarjun7207@gmail.com>
2025-07-24 20:14:29 +0530

    Fix ECS autoscaling target to use actual service name

--- terraform/modules/compute/autoscaling.tf
+++ terraform/modules/compute/autoscaling.tf
@@ -1,30 +1,30 @@
-
-
 resource "aws_appautoscaling_target" "ecs" {
   max_capacity       = var.max_capacity
   min_capacity       = var.min_capacity
-  resource_id        = "service/${var.ecs_cluster_name}/${var.ecs_service_name}"
+  resource_id        = "service/${var.ecs_cluster_name}/${aws_ecs_service.app.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 
   depends_on = [aws_ecs_service.app]
 }
 
+
 resource "aws_appautoscaling_policy" "scale_up" {
-  name               = "cpu-scale-up"
+  name               = "scale-up"
   policy_type        = "TargetTrackingScaling"
   resource_id        = aws_appautoscaling_target.ecs.resource_id
   scalable_dimension = aws_appautoscaling_target.ecs.scalable_dimension
   service_namespace  = aws_appautoscaling_target.ecs.service_namespace
 
   target_tracking_scaling_policy_configuration {
+    target_value       = var.cpu_target_utilization
     predefined_metric_specification {
       predefined_metric_type = "ECSServiceAverageCPUUtilization"
     }
-    target_value       = var.cpu_target_utilization
-    scale_in_cooldown  = 60
     scale_out_cooldown = 60
-
-
+    scale_in_cooldown  = 60
   }
+
+  depends_on = [aws_appautoscaling_target.ecs]
 }
+
